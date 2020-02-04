4a793cb8fbb29015569b3e0ed91bfcb4502a912f
aljarilla <aljarilla.jose1998@gmail.com>
2020-02-04 16:58:31 +0100

    Implementar ayuda salto en unity

---
+++ Origami_Game/Assets/Scripts/Ayuda.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ayuda : MonoBehaviour
+{
+    public SpriteRenderer fotoJump;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Pito");
+        if(collision.transform.name == "Paper Boy")
+        {
+            fotoJump.enabled = true;
+            Debug.Log("Pene");
+        }
+    }
+}
