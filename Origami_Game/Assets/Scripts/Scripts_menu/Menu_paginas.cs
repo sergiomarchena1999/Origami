90b7ceec4f57e0d88ddfa4430a5b8e4dc5dec1d0
pmarinr <pmarinr@gmail.com>
2020-02-05 16:41:06 +0100

    Menu Páginas

--- Origami_Game/Assets/Scripts/Scripts_menu/Menu_paginas.cs
+++ Origami_Game/Assets/Scripts/Scripts_menu/Menu_paginas.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Menu_opciones : MonoBehaviour
+public class Menu_paginas : MonoBehaviour
 {
-    public GameObject menuOpciones;
+   
 
 
-    public void Volver()
-    {
-        SceneManager.UnloadSceneAsync("Menu opciones");
-    }
+  
     public void Continuar()
     {
         //guardar las opciones que se han usado
-        SceneManager.UnloadSceneAsync("Menu opciones");
+        SceneManager.UnloadSceneAsync("Menu paginas");
 
     }
 }
