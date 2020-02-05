d13f4216fb0ca5b57f7129bf56483e8653a32c8d
javikr98 <javieglo98@gmail.com>
2020-02-05 15:52:43 +0100

    Cargar siguiente escena

--- Origami_Game/Assets/Scripts/ControlEscena.cs
+++ Origami_Game/Assets/Scripts/ControlEscena.cs
@@ -16,11 +16,12 @@ public class ControlEscena : MonoBehaviour
     {
         
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("FinEscena"))
+        if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene("1");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
