671551703735cde4de1ff4e2c4866efeeb09eaf3
DavidMFern <43940445+DavidMFern@users.noreply.github.com>
2020-02-11 11:39:27 +0100

    Cambios Menú de Opciones
    -Añadido Audiomixer, y distintos canales
    "Master", "Musica" y "Sonido".
    
    -Cambiado script "Menu_opciones" por "Menu_Opciones"
    
    -Añadido al script "Menu_Opciones" scripts para controlar opciones de los sliders.(WIP)

--- Origami_Game/Assets/Scripts/Scripts_menu/Menu_opciones.cs
+++ Origami_Game/Assets/Scripts/Scripts_menu/Menu_opciones.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.Audio;
 
-public class Menu_opciones : MonoBehaviour
+public class Menu_Opciones : MonoBehaviour
 {
     public GameObject menuOpciones;
-    void Start()
-    {
-    }
+    
 
+
+    // Valorespara el slider
+    public AudioMixer mixer;
+
+    
+
+
+
+    //////////////////////////////////////
     public void Volver()
     {
         SceneManager.UnloadSceneAsync("Menu opciones");
@@ -21,4 +28,22 @@ public class Menu_opciones : MonoBehaviour
         SceneManager.UnloadSceneAsync("Menu opciones");
 
     }
+
+    ///////////////////////////// Datos para canvas ///////////////////////////
+
+    public void VolumenSliderMaster(float sliderInput)
+    {
+        mixer.SetFloat("Master", Mathf.Log10(sliderInput) * 20);
+    }
+
+    public void VolumenSliderMusica(float sliderInput)
+    {
+        mixer.SetFloat("Musica", Mathf.Log10(sliderInput) * 20);
+    }
+
+    public void VolumenSliderEfectos(float sliderInput)
+    {
+        mixer.SetFloat("Efectos", Mathf.Log10(sliderInput) * 20);
+    }
+
 }
