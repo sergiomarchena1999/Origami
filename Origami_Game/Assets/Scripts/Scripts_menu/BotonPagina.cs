f76e55903ae25aa2b8302d6810d1751582b7cbc9
javikr98 <javieglo98@gmail.com>
2020-02-12 16:28:21 +0100

    Menu paginas

--- Origami_Game/Assets/Scripts/Scripts_menu/BotonPagina.cs
+++ Origami_Game/Assets/Scripts/Scripts_menu/BotonPagina.cs
@@ -9,17 +9,21 @@ public class BotonPagina : MonoBehaviour
 
     Text textopagina;
     Text cabeceraPagina;
+    [Tooltip("Escribir aquí la cabecera de la página")]
     public string cabecera;
+    [Tooltip("Escribir aquí el contenido de la página")]
     [TextArea]
     public string texto;   
     GameObject panelPagina;
     Animator controlPanel;
+    Scrollbar barra;
 
     
     void Start()
     {     
         panelPagina = GameObject.Find("PanelInfoPaginas");
         controlPanel = GameObject.Find("PanelInfoPaginas").GetComponent<Animator>();
+        barra = GameObject.Find("ScrollTexto").GetComponent<Scrollbar>();
     }
 
     void Update()
@@ -32,6 +36,7 @@ public class BotonPagina : MonoBehaviour
         controlPanel.SetBool("Mostrar", true);
         textopagina = GameObject.Find("TextoInfoPaginas").GetComponent<Text>();
         textopagina.text = texto;
+        barra.value = 1;
         cabeceraPagina = GameObject.Find("CabeceraInfoPaginas").GetComponent<Text>();
         cabeceraPagina.text = cabecera;
     }
