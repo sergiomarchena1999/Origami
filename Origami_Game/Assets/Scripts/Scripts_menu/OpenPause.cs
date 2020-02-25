19e8302e7f028a72e9e58d7addd77100430f671d
DavidMFern <43940445+DavidMFern@users.noreply.github.com>
2020-02-25 18:16:21 +0100

    Gamemmanager, Menu de pausa , y bugs varios
    -Menu de pausa para testing.
    -Arreglado e integrado script menu de pausa.
    -Intagrado salir al menu principal.
    -Problemas con el menu de pausa, bugs, sale mientras estas en menu de opciones,
    -Y al entra a menus desde el de pausa, salen fondos erroneos.

---
+++ Origami_Game/Assets/Scripts/Scripts_menu/OpenPause.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenPause : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)&& GameManager.pausa == false)
+        {
+            GameManager.pausa = true;
+            SceneManager.LoadScene("Menu pausa", LoadSceneMode.Additive);
+        }
+    }
+}
