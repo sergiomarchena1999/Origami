60f037a5c31f300d228449df39e276ea3cdf75ab
sergiomarchena1999 <55439101+sergiomarchena1999@users.noreply.github.com>
2020-02-27 13:50:35 +0100

    Rata Buggeada

--- Origami_Game/Assets/Scripts/Bosses/Boss_Rata.cs
+++ Origami_Game/Assets/Scripts/Bosses/Boss_Rata.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class Boss_Rata : MonoBehaviour
 {
+    Animator _anim;
+    Rigidbody2D _rb;
+
+    public Transform destIzq;
+    public Transform destDer;
+
     void Start()
     {
-        
+        _anim = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+
+        transform.position = destDer.position;
     }
     
     void Update()
     {
-        
+        Debug.Log(new Vector2(destDer.position.x, destDer.position.y));
+    }
+
+    void Embestida()
+    {
+        Vector2 auxPos = transform.position;
+
+        if (auxPos == new Vector2(destDer.position.x, destDer.position.y))
+        {
+
+        }
+    }
+
+    void Ataque1()
+    {
+
+    }
+
+    void Muerte()
+    {
+
     }
 }
