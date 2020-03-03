3faec262dd449d9e23cb368f67d379fe35c02dbe
sergiomarchena1999 <55439101+sergiomarchena1999@users.noreply.github.com>
2020-03-03 18:09:46 +0100

    Embestida Rata

--- Origami_Game/Assets/Scripts/Bosses/Boss_Rata.cs
+++ Origami_Game/Assets/Scripts/Bosses/Boss_Rata.cs
@@ -10,32 +10,108 @@ public class Boss_Rata : MonoBehaviour
     public Transform destIzq;
     public Transform destDer;
 
+    public float velocidad = 15f;
+    public float tiempoEntreEmbestidas = 4f;
+
+    float random;
+
+    float _timerEmbestida = 0;
+    float _timerAtaque = 0;
+    bool _embestir = false;
+    bool _meCagoEnLaPuta = false;
+    bool _miraDerecha = false;
+    bool _enExtremo = false;
+
     void Start()
     {
-        _anim = GetComponent<Animator>();
+        _anim = GetComponentInChildren<Animator>();
         _rb = GetComponent<Rigidbody2D>();
 
         transform.position = destDer.position;
+
+        _timerAtaque = Random.Range(5, 10);
     }
     
     void Update()
     {
-        Debug.Log(new Vector2(destDer.position.x, destDer.position.y));
+        if (!_miraDerecha)
+            transform.rotation = Quaternion.identity;
+        else
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+
+        if(Time.time > _timerAtaque)
+        {
+            random = Random.Range(-10.0f, 10.0f);
+
+            if (random >= 0)
+            {                
+                _embestir = true;
+
+                if(_embestir && !_meCagoEnLaPuta)
+                {
+                    _timerEmbestida = Time.time;
+                    _timerEmbestida += tiempoEntreEmbestidas;
+                    _meCagoEnLaPuta = true;
+                }
+            }
+            else
+            {
+                Ataque1();
+            }
+            _timerAtaque = Time.time + Random.Range(5, 10);
+        }
+
+        if(_meCagoEnLaPuta)
+        {
+            _enExtremo = false;
+            Embestida();
+        }
     }
 
     void Embestida()
     {
-        Vector2 auxPos = transform.position;
+        Debug.Log("Embisto");
+        Debug.Log(_timerEmbestida);
+        _anim.SetBool("Run", _embestir);
+        
+        
 
-        if (auxPos == new Vector2(destDer.position.x, destDer.position.y))
+        if (transform.position.x == destDer.position.x && _embestir)
         {
+            _miraDerecha = false;
+            _enExtremo = true;
+        }
 
+        if (transform.position.x == destIzq.position.x && _embestir)
+        {
+            _miraDerecha = true;
+            _enExtremo = true;
+        }
+
+        if (_embestir && _miraDerecha)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, destDer.position, velocidad * Time.deltaTime);            
+        }
+
+        if (_embestir && !_miraDerecha)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, destIzq.position, velocidad * Time.deltaTime);            
+        }
+
+        if (Time.time > _timerEmbestida && (transform.position.x == destDer.position.x || transform.position.x == destIzq.position.x))
+        {
+            Debug.Log("No embisto");
+            _embestir = false;
+            _meCagoEnLaPuta = false;
+            _anim.SetBool("Run", _embestir);
+            _miraDerecha = !_miraDerecha;
         }
     }
 
     void Ataque1()
     {
-
+        Debug.Log("Ataco 1");
+        _anim.SetTrigger("Ataque1");
     }
 
     void Muerte()
